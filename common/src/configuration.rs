4ed664fb3620d4715a5473381f0b381f66066229
VickyA5 <vavalos@fi.uba.ar>
2025-06-16 17:44:25 -0300

    docs: log fix

--- common/src/configuration.rs
+++ common/src/configuration.rs
@@ -16,8 +16,21 @@ pub struct SingleCustomerInfo {
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct SingleRiderInfo {
+    pub id: u32,
+    pub port: u32,
+    pub x: u16,
+    pub y: u16
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct PedidosRustConfig {
     pub infos: Vec<SinglePedidosRustInfo>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct RiderConfig {
+    pub infos: Vec<SingleCustomerInfo>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -29,6 +42,7 @@ pub struct CustomerConfig {
 pub struct Configuration {
     pub pedidos_rust: PedidosRustConfig,
     pub customer: CustomerConfig,
+    pub rider: RiderConfig,
 }
 
 impl Configuration {
