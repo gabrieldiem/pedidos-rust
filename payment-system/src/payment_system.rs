0e6a0ea4ed8d8d184997174003318961a206a6a4
VickyA5 <vavalos@fi.uba.ar>
2025-06-17 00:03:40 -0300

    fix: gateway de pagos tambien se conecta bien a pedidos rust

--- payment-system/src/payment_system.rs
+++ payment-system/src/payment_system.rs
@@ -2,31 +2,29 @@ use actix::{Actor, ActorContext, AsyncContext, Context, Handler};
 use actix::{Addr, Message, StreamHandler};
 use actix_async_handler::async_handler;
 use common::configuration::Configuration;
-use common::constants::{MAX_ORDER_DURATION, MIN_ORDER_DURATION, ORDER_REJECTED_PROBABILITY};
-use common::protocol::{Location, SocketMessage, Stop};
+use common::constants::{DEFAULT_PAYMENT_PORT, PAYMENT_DURATION, PAYMENT_REJECTED_PROBABILITY};
+use common::protocol::{SocketMessage, Stop};
 use common::tcp::tcp_connector::TcpConnector;
 use common::tcp::tcp_message::TcpMessage;
 use common::tcp::tcp_sender::TcpSender;
 use common::utils::logger::Logger;
-use rand::Rng;
+use rand::random;
 use std::io;
-use std::time::Duration;
 use tokio::io::{AsyncBufReadExt, BufReader, split};
+use tokio::time::sleep;
 use tokio_stream::wrappers::LinesStream;
 
 #[allow(dead_code)]
-pub struct Restaurant {
+pub struct PaymentSystem {
     tcp_sender: Addr<TcpSender>,
     logger: Logger,
-    location: Location,
     config: Configuration,
     my_port: u32,
     peer_port: u32,
     tcp_connector: Addr<TcpConnector>,
-    name: String,
 }
 
-impl Actor for Restaurant {
+impl Actor for PaymentSystem {
     type Context = Context<Self>;
 }
 
@@ -36,46 +34,49 @@ pub struct Start;
 
 #[derive(Message, Debug)]
 #[rtype(result = "()")]
-pub struct InformLocation;
+pub struct RegisterPaymentSystem;
 
 #[derive(Message, Debug)]
 #[rtype(result = "()")]
-pub struct PrepareOrder {
+pub struct ExecuteAutorization {
     pub customer_id: u32,
-    pub price: f64,
+    pub amount: f64,
+    pub restaurant_name: String,
 }
 
 #[async_handler]
-impl Handler<Start> for Restaurant {
+impl Handler<Start> for PaymentSystem {
     type Result = ();
 
     async fn handle(&mut self, _msg: Start, _ctx: &mut Self::Context) -> Self::Result {
-        _ctx.address().do_send(InformLocation {});
+        _ctx.address().do_send(RegisterPaymentSystem {});
     }
 }
 
 #[async_handler]
-impl Handler<Stop> for Restaurant {
+impl Handler<Stop> for PaymentSystem {
     type Result = ();
 
     async fn handle(&mut self, _msg: Stop, _ctx: &mut Self::Context) -> Self::Result {
-        self.logger.debug("Stopping Restaurant");
+        self.logger.debug("Stopping PaymentSystem");
         _ctx.stop();
         std::process::exit(0);
     }
 }
 
 #[async_handler]
-impl Handler<InformLocation> for Restaurant {
+impl Handler<RegisterPaymentSystem> for PaymentSystem {
     type Result = ();
 
-    async fn handle(&mut self, _msg: InformLocation, _ctx: &mut Self::Context) -> Self::Result {
-        self.logger.debug(&format!(
-            "Informing my location to PedidosRust: ({}, {})",
-            self.location.x, self.location.y
-        ));
+    async fn handle(
+        &mut self,
+        _msg: RegisterPaymentSystem,
+        _ctx: &mut Self::Context,
+    ) -> Self::Result {
+        self.logger
+            .debug("Registering Payment System to PedidosRust...");
 
-        let msg = SocketMessage::InformLocation(self.location, self.name.clone());
+        let msg = SocketMessage::RegisterPaymentSystem;
         if let Err(e) = self.send_message(&msg) {
             self.logger.error(&e.to_string());
             return;
@@ -84,88 +85,62 @@ impl Handler<InformLocation> for Restaurant {
 }
 
 #[async_handler]
-impl Handler<PrepareOrder> for Restaurant {
+impl Handler<ExecuteAutorization> for PaymentSystem {
     type Result = ();
 
-    async fn handle(&mut self, msg: PrepareOrder, _ctx: &mut Self::Context) {
+    async fn handle(&mut self, msg: ExecuteAutorization, _ctx: &mut Self::Context) {
         self.logger.info(&format!(
-            "Order received from customer {} with price {}",
-            msg.customer_id, msg.price
+            "Executing authorization for client {} with price {}...",
+            msg.customer_id, msg.amount
         ));
+        let authorized = random::<f32>() > PAYMENT_REJECTED_PROBABILITY;
 
-        let response = SocketMessage::OrderInProgress(msg.customer_id);
+        let response = if authorized {
+            SocketMessage::PaymentAuthorized(msg.customer_id, msg.amount, msg.restaurant_name)
+        } else {
+            SocketMessage::PaymentDenied(msg.customer_id, msg.amount, msg.restaurant_name)
+        };
+        self.logger
+            .info(&format!("Response to authorization: {:?}", response));
         if let Err(e) = self.send_message(&response) {
             self.logger.error(&e.to_string());
-            return;
         }
-
-        let secs = rand::rng().random_range(MIN_ORDER_DURATION..=MAX_ORDER_DURATION);
-        _ctx.notify_later(
-            ContinueOrder {
-                customer_id: msg.customer_id,
-                price: msg.price,
-            },
-            Duration::from_secs(secs),
-        );
     }
 }
 
 #[derive(Message)]
 #[rtype(result = "()")]
-pub struct ContinueOrder {
+pub struct ExecutePayment {
     pub customer_id: u32,
-    pub price: f64,
+    pub amount: f64,
 }
 
 #[async_handler]
-impl Handler<ContinueOrder> for Restaurant {
+impl Handler<ExecutePayment> for PaymentSystem {
     type Result = ();
 
-    async fn handle(&mut self, msg: ContinueOrder, _ctx: &mut Self::Context) {
-        let accepted = rand::random::<f32>() > ORDER_REJECTED_PROBABILITY;
-
-        if !accepted {
-            self.logger.info(&format!(
-                "Order from client {} rejected due to lack of stock",
-                msg.customer_id
-            ));
-            let response = SocketMessage::OrderCalcelled(msg.customer_id);
-            if let Err(e) = self.send_message(&response) {
-                self.logger.error(&e.to_string());
-            }
-            return;
-        }
-
+    async fn handle(&mut self, msg: ExecutePayment, _ctx: &mut Self::Context) {
+        self.logger.info("Executing payment...");
+        sleep(std::time::Duration::from_secs(PAYMENT_DURATION)).await;
+        let response = SocketMessage::PaymentExecuted(msg.customer_id, msg.amount);
         self.logger.info(&format!(
-            "Order from client {} with price {} is ready",
-            msg.customer_id, msg.price
+            "Payment executed for client {} with amount {}",
+            msg.customer_id, msg.amount
         ));
-        let response = SocketMessage::OrderReady(msg.customer_id, self.location);
         if let Err(e) = self.send_message(&response) {
             self.logger.error(&e.to_string());
         }
     }
 }
 
-impl Restaurant {
-    pub async fn new(
-        id: u32,
-        logger: Logger,
-    ) -> Result<Addr<Restaurant>, Box<dyn std::error::Error>> {
+impl PaymentSystem {
+    pub async fn new(logger: Logger) -> Result<Addr<PaymentSystem>, Box<dyn std::error::Error>> {
         logger.info("Starting...");
 
         // Setting up ports
         let config = Configuration::new()?;
-        let port_pair = config.restaurant.infos.iter().find(|pair| pair.id == id);
 
-        let port_pair = match port_pair {
-            Some(pair) => pair,
-            None => {
-                return Err(format!("Could not find port in configuration for id: {}", id).into());
-            }
-        };
-
-        let my_port = port_pair.port;
+        let my_port = DEFAULT_PAYMENT_PORT;
         let dest_ports: Vec<u32> = config
             .pedidos_rust
             .infos
@@ -180,17 +155,17 @@ impl Restaurant {
         let peer_port = peer_address.port();
 
         // Creating actor
-        let restaurant = Restaurant::create(|ctx| {
+        let payment_system = PaymentSystem::create(|ctx| {
             let (read_half, write_half) = split(stream);
 
-            Restaurant::add_stream(LinesStream::new(BufReader::new(read_half).lines()), ctx);
+            PaymentSystem::add_stream(LinesStream::new(BufReader::new(read_half).lines()), ctx);
 
             let tcp_sender = TcpSender {
                 write_stream: Some(write_half),
             }
             .start();
 
-            logger.debug("Created Restaurant");
+            logger.debug("Created Payment System");
 
             let tcp_connector_actor = TcpConnector::create(|_tcp_ctx| TcpConnector {
                 logger: Logger::new(Some("[TCP-CONNECTOR]")),
@@ -198,39 +173,36 @@ impl Restaurant {
                 dest_ports: dest_ports.clone(),
             });
 
-            let restaurant_info = match config.restaurant.infos.iter().find(|c| c.id == id) {
-                Some(info) => info,
-                None => {
-                    panic!("No se encontró el cliente con id: {}", id);
-                }
-            };
-
-            let restaurant_location = Location::new(restaurant_info.x, restaurant_info.y);
-            logger.info(&format!(
-                "Restaurant {} started at port {} with location ({}, {})",
-                id, my_port, restaurant_location.x, restaurant_location.y
-            ));
-            Restaurant {
+            logger.info(&format!("Gateway started at port {}", my_port));
+            PaymentSystem {
                 tcp_sender,
-                logger: Logger::new(Some("[RESTAURANT]")),
-                location: restaurant_location,
+                logger: Logger::new(Some("[PAYMENT-SYSTEM]")),
                 config: config.clone(),
                 my_port,
                 peer_port: peer_port as u32,
                 tcp_connector: tcp_connector_actor,
-                name: restaurant_info.name.clone(),
             }
         });
-        Ok(restaurant)
+        Ok(payment_system)
     }
 
     #[allow(unreachable_patterns)]
-    fn dispatch_message(&mut self, line_read: String, ctx: &mut <Restaurant as Actor>::Context) {
+    fn dispatch_message(&mut self, line_read: String, ctx: &mut <PaymentSystem as Actor>::Context) {
         let parsed_line = serde_json::from_str(&line_read);
         match parsed_line {
             Ok(message) => match message {
-                SocketMessage::PrepareOrder(customer_id, price) => {
-                    ctx.address().do_send(PrepareOrder { customer_id, price });
+                SocketMessage::AuthorizePayment(customer_id, amount, restaurant_name) => {
+                    ctx.address().do_send(ExecuteAutorization {
+                        customer_id,
+                        amount,
+                        restaurant_name,
+                    });
+                }
+                SocketMessage::ExecutePayment(customer_id, amount) => {
+                    ctx.address().do_send(ExecutePayment {
+                        customer_id,
+                        amount,
+                    });
                 }
                 _ => {
                     self.logger
@@ -264,7 +236,7 @@ impl Restaurant {
     }
 }
 
-impl StreamHandler<Result<String, io::Error>> for Restaurant {
+impl StreamHandler<Result<String, io::Error>> for PaymentSystem {
     fn handle(&mut self, msg_read: Result<String, io::Error>, ctx: &mut Self::Context) {
         match msg_read {
             Ok(line_read) => match line_read.strip_suffix("\n") {
